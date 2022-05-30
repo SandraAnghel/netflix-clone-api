e526b66b746cf180ea358cae619382064f73dc9a
sandraanghel <1815743@student.uwtsd.ac.uk>
2022-05-30 16:55:17 +0100

    Add style on row component

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
-import Row from './Row';
-import request from './requests';
+import "./App.css";
+import Row from "./Row";
+import request from "./requests";
 
 function App() {
   return (
     <div className="App">
-      <Row title="NETFLIX ORIGINALS" fetchUrl={request.fetchNetflixOriginals}/>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={request.fetchNetflixOriginals}
+        isLargeRow
+      />
       <Row title="Trending Now" fetchUrl={request.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={request.fetchTopRated} />
+      <Row title="Actions Movies" fetchUrl={request.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={request.fetchDocumentaries} />
     </div>
   );
 }
