46e4bc8695e2da018e76f3ad988dda982050869f
sandraanghel <1815743@student.uwtsd.ac.uk>
2022-05-31 14:18:55 +0100

    Create and design nav component

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import Row from "./Row";
 import request from "./requests";
 import Banner from "./Banner";
+import Nav from "./Nav";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <Nav />
       <Banner />
 
       <Row
